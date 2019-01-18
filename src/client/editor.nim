b2c2685f594c5aca7e4d8304b56347547fd51099
Nicolas Peckman <nicolas.peckman@sas.com>
2019-01-18 17:27:52 -0500

    Replaced monaco with ace

--- src/client/editor.nim
+++ src/client/editor.nim
@@ -1,79 +1,19 @@
 import jsffi, dom
 import karax/kbase
 import ../common/file
-{. emit: """
-import 'monaco-editor/esm/vs/editor/browser/controller/coreCommands.js';
-// import 'monaco-editor/esm/vs/editor/browser/widget/codeEditorWidget.js';
-import 'monaco-editor/esm/vs/editor/contrib/find/findController.js';
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
 
-// import 'monaco-editor/esm/vs/language/typescript/monaco.contribution';
-// import 'monaco-editor/esm/vs/language/css/monaco.contribution';
-// import 'monaco-editor/esm/vs/language/json/monaco.contribution';
-// import 'monaco-editor/esm/vs/language/html/monaco.contribution';
-// import 'monaco-editor/esm/vs/basic-languages/bat/bat.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/coffee/coffee.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/cpp/cpp.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/csharp/csharp.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/csp/csp.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/css/css.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/dockerfile/dockerfile.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/fsharp/fsharp.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/go/go.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/handlebars/handlebars.contribution.js';
-import 'monaco-editor/esm/vs/basic-languages/html/html.contribution.js';
-import 'monaco-editor/esm/vs/basic-languages/ini/ini.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/java/java.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/less/less.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/lua/lua.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/markdown/markdown.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/msdax/msdax.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/mysql/mysql.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/objective-c/objective-c.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/pgsql/pgsql.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/php/php.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/postiats/postiats.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/powershell/powershell.contribution.js';
-import 'monaco-editor/esm/vs/basic-languages/pug/pug.contribution.js';
-import 'monaco-editor/esm/vs/basic-languages/python/python.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/r/r.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/razor/razor.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/redis/redis.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/redshift/redshift.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/ruby/ruby.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/sb/sb.contribution.js';
-import 'monaco-editor/esm/vs/basic-languages/scss/scss.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/solidity/solidity.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/sql/sql.contribution.js';
-import 'monaco-editor/esm/vs/basic-languages/swift/swift.contribution.js';
-// import 'monaco-editor/esm/vs/basic-languages/vb/vb.contribution.js';
-import 'monaco-editor/esm/vs/basic-languages/xml/xml.contribution.js';
-import 'monaco-editor/esm/vs/basic-languages/yaml/yaml.contribution.js';
-import 'monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution';
-import 'monaco-editor/esm/vs/basic-languages/typescript/typescript.contribution';
-"""
-.}
+let ace = require("ace-builds/src-noconflict/ace")
+discard require("ace-builds/src-noconflict/theme-monokai")
+discard require("ace-builds/src-noconflict/mode-javascript")
 
 var editor: JsObject
-var monaco {. nodecl, importc .}: JsObject
-var self {. nodecl, importc .}: JsObject
-
-proc newWorker(path: cstring): JsObject {. nodecl, importcpp: "new Worker(@)" .}
-
-proc getWorker(moduleId: cstring, label: cstring): JsObject =
-  case $label
-  of "json": newWorker(cstring"../../node_modules/monaco-editor/esm/vs/language/json/json.worker.js")
-  of "css": newWorker(cstring"../../node_modules/monaco-editor/esm/vs/language/css/css.worker.js")
-  of "html": newWorker(cstring"../../node_modules/monaco-editor/esm/vs/language/html/html.worker.js")
-  of "typescript", "javascript": newWorker(cstring"../../node_modules/monaco-editor/esm/vs/language/typescript/ts.worker.js")
-  else: newWorker(cstring"../../node_modules/monaco-editor/esm/vs/editor/editor.worker.js")
 
 proc setupEditor*() =
-  var env = newJsObject()
-  env["getWorker"] = getWorker
-  editor = monaco.editor.create(document.getElementById(cstring"editor-element"))
+  ace.config.set(cstring"basePath", cstring"client")
+  editor = ace.edit("editor-element")
+  editor.setTheme("ace/theme/monokai")
+  editor.session.setMode("ace/mode/javascript")
   
 proc updateEditor*(file: FileData) = 
-  editor.getModel().setValue(file.text)
-  monaco.editor.setModelLanguage(editor.getModel(), file.lang)
-proc getEditorText*(): kstring = editor.getModel().getValue().to(kstring)
+  editor.setValue(file.text, -1)
+proc getEditorText*(): kstring = editor.getValue().to(kstring)
